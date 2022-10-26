3708fc36ac0bd4b31f6ab32a58a038eb8c74a440
Antoine Montier <115585369+antoineMontier@users.noreply.github.com>
2022-10-26 20:48:20 +0200

    tested system and other positive() function
    working

--- Tests.java
+++ Tests.java
@@ -17,14 +17,74 @@ public class Tests {
     f.add(150, 25);
     f.add(4, 13);
 
+    double c[][] = new double[5][5];
+
+
+    c[0][0] = 2;
+    c[0][1] = 1;
+    c[0][2] = -1;
+    c[0][3] = 4;
+    c[0][4] = -19;
+
+    c[1][0] = -1;
+    c[1][1] = -2;
+    c[1][2] = 1;
+    c[1][3] = 1;
+    c[1][4] = 3;
+
+    c[2][0] = 2;
+    c[2][1] = 4;
+    c[2][2] = 2;
+    c[2][3] = 1;
+    c[2][4] = -25;
+
+    c[3][0] = -1;
+    c[3][1] = 1;
+    c[3][2] = -1;
+    c[3][3] = -2;
+    c[3][4] = -5;
+
+    LinearSystem s = new LinearSystem(c);
+    //-9.0666 9.266 -0.399 6.866
+
+    
+    System.out.println(s);
+
+    s.scale();
+
+    System.out.println(s);
+
+    System.out.println(s.solutions());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     f.sort();
     System.out.println("");
-    System.out.println("f(x) = " + f + "  f is positive on : " + f.positive());
+    
     System.out.println("");
 
 
 
     /*
+    System.out.println("f(x) = " + f + "  f is positive on : " + f.positive());
+
     System.out.println("f(x) = " + f + "  roots of f : " + f.roots());
 
     System.out.println("f(x) = " + f + "  f even : " + f.even());
